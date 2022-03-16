f54ea426c1ffc72584b51bebd8075a2d5d2f7705
kokkoxPL <adriangamracy@gmail.com>
2022-03-16 23:45:19 +0100

    Poprawa

--- DniOtwarteAdmin/php/wyniki.php
+++ DniOtwarteAdmin/php/wyniki.php
@@ -1,58 +1,23 @@
 <?php
-function odp($ans) {
-  if($ans == "1") {
-    return 1;
-  }
-    return 0;
-}
-
 $server = "localhost";
 $username = "root";
 $password = "";
 $baza = "dni_otwarte";
-$tabela = "uczniowie";
-$cookie = "Cookies";
+$tabela = "wyniki";
 $wynikiNick = [];
 $wynikiWynik = [];
-$wyn;
+
 $sql = new mysqli($server, $username, $password);
 $sql->query("CREATE DATABASE IF NOT EXISTS $baza;");
 $sql = new mysqli($server, $username, $password, $baza);
-$sql->query("CREATE TABLE IF NOT EXISTS $tabela(id int AUTO_INCREMENT PRIMARY KEY, dane varchar(20), nick varchar(20), szkola varchar(40), miasto varchar(20), wynik int);");
-
-
-if($_SERVER["REQUEST_METHOD"] == "POST") {
-  $dane = $_POST["dane"];
-  $nick = $_POST["nick"];
-  $szkola = $_POST["szkola"];
-  $miasto = $_POST["miasto"];
-  $wynik = 0;
-  for ($i = 1; $i <= 10; $i++) {
-    if (isset($_POST["answer".$i])) {
-      $wynik += odp($_POST["answer".$i]);
-    };
-  };
-
-
-  $sql->query("INSERT INTO $tabela(dane, nick, szkola, miasto, wynik) VALUES('$dane', '$nick', '$szkola', '$miasto', '$wynik');");
-
-  setcookie($cookie , $sql->insert_id, time() + (3600), "/");
-
-  unset($_POST);
-  header("Location: ".$_SERVER['PHP_SELF']);
-}
+$sql->query("CREATE TABLE IF NOT EXISTS $tabela(id int AUTO_INCREMENT PRIMARY KEY, dane varchar(40), nick varchar(20), szkola varchar(40), miasto varchar(20), wynik int);");
 
 $wyniki = $sql->query("SELECT nick, wynik FROM $tabela ORDER BY wynik DESC");
 while($info = $wyniki->fetch_assoc()) {
   $wynikiNick[] = $info["nick"];
   $wynikiWynik[] = $info["wynik"];
 }
-/*$a = $_COOKIE['$cookie']
 
-$wyniki2 = $sql->query("SELECT wynik FROM $tabela WHERE id = 2");
-while($info2 = $wyniki2->fetch_assoc()) {
-  $wyn[] = $info2["wynik"];
-}*/
 $sql->close();
 ?>
 
